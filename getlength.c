dc4a6284b073b578f2ad630d48a0d497483870ed
Noah <Franck.noahk@gmail.com>
2018-03-05 20:49:16 +0000

    dr kirk's code from class

--- getlength.c
+++ getlength.c
@@ -3,10 +3,23 @@
 
 int getLength(char str[])
 {
-	
+	int i = 1;
+	toReturn =0;
+	while( i != 0)
+	{
+		i = str[i];
+		toReturn++;
+	}
+	return toReturn;
 }
 
 void main(void)
 {
-	:
+	char arr[100];
+	int len;
+	printf("Enter a string: ");
+	scanf("%s\n",%str);
+	len = getLength(str);
+	printf("%d\n", len);
+	
 }
