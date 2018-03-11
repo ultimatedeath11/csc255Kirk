6b79edc3c7e9435f876e17a1d468f756276b011e
Noah <Franck.noahk@gmail.com>
2018-03-11 18:13:04 +0000

    working on making and randomly assigning the variable into the array, but I need to move the assign character function into the main method

--- necklaces.c
+++ necklaces.c
@@ -3,10 +3,46 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+#define SIZE 10
+
+void printCharacter(char n[], int size)
+{ 
+
+	int i; 
+	for(i = 0; i < size ;i++)
+	{ 
+
+		printf(" %c\t", n[i]);	
+
+	}
+	printf("\n");
+}
+
+char[] assignCharacter(char n[], int size)
+{ 
+
+	int i;
+	for(i = 0; i < size;i++){
+		n[i] = 'a' * rand() % 26;
+
+		if(n[i] != "w" || n[i] != 'b' || n[i] != 'r')
+		{ 
+
+			i= i - 1; 
+
+		 }
+
+	}
+
+ }
 
 void main(void)
 { 
 
-
+	int i;
+	char static neck[SIZE];
+	printCharacter(assignCharacter(neck,SIZE), SIZE);
+	getchar();
+	getchar();
 
 }
