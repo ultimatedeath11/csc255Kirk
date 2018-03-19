4f35c8921d126f4d2b40ef097397698a60df2e83
Noah <Franck.noahk@gmail.com>
2018-03-19 19:45:40 +0000

    switched from manual entry to random and now sorts it with quick sorts

--- integers.c
+++ integers.c
@@ -5,26 +5,63 @@
 #include <stdlib.h>
 #define SIZE 100
 
+int compareFloats(void *a, void *b);
+
 void main(void) 
 { 
 	//going to enter number of floats to enter 
 	//dynamic arrays
 	int count;
-	int *myArray;
+	float *myArray;
 	int i;
-	printf("ENter the number of floats to enter: \n");
+	printf("Enter the number of floats to enter: \n");
 	scanf(" %d", &count);
 	myArray = malloc(count * sizeof(float));// allocates this many size of array 	it returns the starting address of the array
+	/* for numbers from the keyboard
 	for(i = 0; i < count;i++)
 	{
  		printf("Float #%d? ", i+1);
 		scanf(" %f", &myArray[i]);
 	}
+	*/
+	srand(time(NULL));
+	for(i = 0; i < count ;i++)
+	{
+		myArray[i] = rand() % 100 + 1.0 ;
+		srand(rand());
+	}
 	for(i = 0; i < count;i++)
 	{
- 		printf("Float #%d = %.2f\n", i+1, myArray[i]);
+		if(i % 5 == 0)
+			printf("\n");
+ 		printf("Float #%d = %.2f\t", i+1, myArray[i]);
 	}
-	
+	printf("\n");
+	// add to it to be what the difference between any integrrs  	
+	//this is a function that sorts the array	
+	qsort(myArray, count, sizeof(float),compareFloats); 
+		for(i = 0; i < count;i++)
+	{
+		if(i % 5 == 0)
+			printf("\n");
+ 		printf("Float #%d = %.2f\t", i+1, myArray[i]);
+	}
+	printf("\n");
+}
+int compareFloats(void *a, void *b)
+{ 
+
+	float *aa = (float *)a;
+	float *bb = (float *)b;
+
+	if(*aa > *bb)
+		return 1;
+	else if (*aa < *bb)
+		return -1;
+	else 
+		return 0;
 	
 
 }
+
+
